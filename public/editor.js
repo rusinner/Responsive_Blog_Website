a267ae7a7c50b915bc1ca583074ce537939a2902
Theologos Mavropoulos <rusinner88@gmail.com>
2022-10-25 18:41:46 +0300

    promise needs to be fixed

---
+++ public/editor.js
@@ -0,0 +1,32 @@
+const blogTitleField = document.querySelector('.title');
+const articleField = document.querySelector('.article');
+
+//banner
+
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector('.banner');
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const uploadBtn = document.querySelector('#image-upload');
+
+bannerImage.addEventListener('change',() => {
+    uploadImage(bannerImage,'banner');
+});
+
+const uploadImage = (uploadFile , uploadType) => {
+    const [file] = uploadFile.files;
+    if(file && file.type.includes('image')){
+        const formdata = new FormData();
+        formdata.append('image' , file);
+
+        fetch('/upload' , {
+            method: 'post',
+            body:formdata
+        }).then(res => res.json())
+        .then(data => {
+            bannerPath = `${location.origin}/${data}`;
+            banner.style.backgroundImage = `url('${bannerPath}')`;
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
 let initial_path = path.join(__dirname,"public");
 
 const app = express();
 app.use(express.static(initial_path));
+app.use(fileUpload());
 
 app.get('/', (req,res) =>{
     res.sendFile(path.join(initial_path,"home.html"));
 });
+
+app.get('/editor' , (req,res) => {
+    res.sendFile(path.join(initial_path,"editor.html"));
+});
+//upload link
+app.post('/upload', (req,res) => {
+    let file = req.files.image;
+    let date = new Date();
+    //image name
+    let imageName = date.getDate() = date.getTime() + file.name;
+    //image upload path
+    let path = 'public/uploads/' + imageName;
+
+    //create upload
+    file.mv(path , (err ,result) => {
+        if(err){
+            throw err;
+        }else{
+            res.json(`uploads/${imageName}`);
+        }
+    });
+
+
+});
+
 
 app.listen(3000,()=>{
     console.log('Listening...');

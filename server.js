0c2201b3ec17601c5b5eebde5930e6a6def4913d
Theologos Mavropoulos <rusinner88@gmail.com>
2022-10-25 12:55:42 +0300

    section blogs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-let initial_path = path.join(__dirname,"public/uploads");
+let initial_path = path.join(__dirname,"public");
 
 const app = express();
 app.use(express.static(initial_path));
